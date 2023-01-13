8321d64f3550904b25277f47d6aea161c0551aa5
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-13 20:20:58 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,62 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 
 
 
 function App ()  {
 
+const [type, setType] = useState('users');
+// eslint-disable-next-line
+const [data, setData] = useState([]);
+const [pos, setPos] = useState({
+  x: 0, 
+  y: 0
+});
 
+// useEffect(() => {
+// console.log('render');
+// });
+
+useEffect(() => {
+  fetch(`https://jsonplaceholder.typicode.com/${type}`)
+  .then((response) => response.json())
+  .then((json) => setData(json))
+
+  return () => {
+    console.log('clean type');
+
+  }
+  
+}, [type])
+
+const mouseMoveHandler = evt => {
+  setPos({
+    x: evt.clientX,
+    y: evt.clientY,
+  })
+}
+
+useEffect(() => {
+  console.log('componentDidMount');
+
+  window.addEventListener('mousemove', mouseMoveHandler)
+   return () => {
+    window.removeEventListener('mousemove', mouseMoveHandler)
+   }
+}, []);
 
 
   return (
     <div>
-     
+     <h1>Resours: {type}</h1>
+     <button onClick={() => setType('users')}>Users</button>
+     <button onClick={() => setType('todos')}>Todos</button>
+     <button onClick={() => setType('posts')}>Posts</button>
+
+     {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+     <pre>{JSON.stringify(pos, null, 2)}</pre>
     </div>
- 
+   
     
   );
 };
