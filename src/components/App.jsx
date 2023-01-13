9e97c17208ce967230a262c685fae566b2061d0e
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-13 17:14:32 +0200

    useState1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
+import { useState } from "react";
+
+
+function App ()  {
+const [count, setCount] = useState(0);
+
+function increment () {
+  setCount(count + 1)
+}
+
+function discrement () {
+  setCount(count - 1)
+}
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+     <h1>Counter: {count}</h1>
+     <button onClick={increment} className='btn btn-success'>Add</button>
+     <button onClick={discrement} className='btn btn-danger'>Remove</button>
     </div>
+ 
+    
   );
 };
+
+export default App;
