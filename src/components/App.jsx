c9d50e666849a24e4740bca79c5fe60c6c11820f
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-01-14 11:45:45 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,16 @@
-import { useState, useEffect, useRef } from "react";
+// eslint-disable-next-line
+import { useState, useEffect, useRef, useMemo } from "react";
 
 
 
 
 function App ()  {
 
-  // const [renderCount, setRenderCount] = useState(1);
-const [value, setValue] = useState('initial'); 
-const renderCount = useRef(1);
-const inputRef = useRef(null);
-const prevValue = useRef('');
-
-
-
-  useEffect(() => {
-    // setRenderCount(prev => prev + 1)
-    renderCount.current++
-    console.log(inputRef.current.value);
-  });
-
-  useEffect(() => {
-  prevValue.current = value
-  }, [value]);
-
-
-
-  const focus = () => inputRef.current.focus();
+ 
 
   return (
     <div>
-     <h1>Quantity of renders: {renderCount.current}</h1>
-     <h2>Last state: {prevValue.current}</h2>
-     <input ref={inputRef} type="text" onChange={e => setValue(e.target.value)} value={value}/>
-     <button className="btn-success" onClick={focus}>Focus</button>
+     
     </div>
     
   );
